d3b083b760561f28330f929e6983bfdbcb2ee898
Justin1132004 <jmorvay@students.mchenry.edu>
2025-08-26 21:52:48 -0500

    commit javascript HTML assignment

---
+++ addingjavascripttohtml/scripts/scripts.js
@@ -0,0 +1,10 @@
+    function myfunction2(){
+        document.getElementById("sample").innerHTML = 
+        "Changed from a function in the head section, in an external file!";
+    }
+
+    //Single line comment
+
+    /*
+    block comment
+    */
