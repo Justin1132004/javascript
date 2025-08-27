d6ce557f2955e7d06ba4f8af0a84f18f61b627b5
Justin1132004 <jmorvay@students.mchenry.edu>
2025-08-27 17:53:16 -0500

    commit assessment01

---
+++ assessment-01/scripts/scripts.js
@@ -0,0 +1,9 @@
+    function changestartp(){
+        document.getElementById("startp").innerHTML = 
+        "This paragraph was changed on startup";
+    }
+
+    function changestartbutton(){
+        document.getElementById("startp").innerHTML = 
+        "This paragraph was changed by the button above calling an external script";
+    }
